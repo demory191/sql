4265059aa9b3f498ec17569aab98626bcfff8a4e
Adebayo Ademola <demory191@gmail.com>
2016-08-03 13:10:24 +0100

    added sqla.py

---
+++ sqla.py
@@ -0,0 +1,16 @@
+#create a SQLite database and table
+
+#import the sqlite3 library
+import sqlite3
+
+#create a new database if the database dosent already exist
+conn = sqlite3.connect("new.db")
+
+#get a cursor object to execute SQL commands
+cursor = conn.cursor()
+
+#create a table
+cursor.execute("""CREATE TABLE population (city TEXT, state TEXT, population INT)""")
+
+#close connection
+conn.close()
